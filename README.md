b5740b7b7a0e5b330d280beff4df5904f5f2b860
[jesse.galley] <[jesse.galley@hostpapa.com]>
2024-03-31 22:42:13 -0300

    added readme and various TODOs

---
+++ README.md
@@ -0,0 +1,13 @@
+# fiwatcherd
+
+
+## File Integrity Watcher Daemon
+
+A simple lightweight service to watch one or more files for changes, and either alert or take action when they do.
+
+It simply polls the filesystem to do this, keeping state on each watched file in memory.
+
+This tool is not ready to be used by anyone right now, it was made to solve a simple domain-specific problem with a single file that contained an integer value.
+
+In the future I will add the functionality listed in the TODOs to generalize this tool into something more useful.
+
--- fiwatcherd.go
+++ fiwatcherd.go
@@ -12,7 +12,6 @@ import (
 	"time"
 
 	flag "github.com/spf13/pflag"
-	// "golang.org/x/tools/go/analysis/passes/nilfunc"
 )
 
 const (
@@ -20,9 +19,14 @@ const (
   progName = "fiwatcherd"
 )
 
+// TODO: add systemd package to provide systemd ready messages 
+// TODO: attempt to write PID file 
+//       and implement PID file checking logics
+// TODO: allow arbitrary levels of concurrency when testing
+//       multiple files 
 
 var flagVersion bool
-// var flagVerbose bool
+// var flagVerbose bool  //TODO: quiet INFO logs
 var flagDebug bool
 var flagFix bool
 var flagIncrement  bool
@@ -42,6 +46,7 @@ func setupCliArgs () {
   flag.BoolVarP(&flagIncrement, "increment", "i", false, "if file is reverted with -F/--fix, also increment it")  
   flag.BoolVarP(&flagDebug, "debug", "D", false, "debug output")  
   flag.IntVarP(&flagTickrate, "tickrate", "T", 1000, "service tickrate in millseconds")  
+  // TODO: add a field for a list of input files/STDIN list of files
   flag.Parse()
 
   // if -v/--version is given, print version info and exit
@@ -51,6 +56,8 @@ func setupCliArgs () {
   }
 
   // make sure that an incorrect number of args wasn't provided
+  // TODO: if input file list/STDIN is provided then we should
+  //       accept 0 args instead of 1 arg 
   expectedArgs := 1
   if len(flag.Args()) != expectedArgs {
     flag.Usage()
@@ -87,12 +94,18 @@ func main() {
   for {
     select {
     case <-ticker.C:
+      // TODO: refactor this main loop to execute N "tests"
+      // the tests should maybe be structs with a test interface
+      // and accept a function param
+      // this would clean up all this hardcoded shit and allow 
+      // for each test object to be run concurrently (if desired)
       // try to stat the file
       fi, err := os.Stat(argFilename)
       if err != nil {
         slog.Error("stat error", "err", err)
         continue
       }
+
       contents := ""
       fc, err := os.ReadFile(argFilename)
       if err != nil {
@@ -101,16 +114,33 @@ func main() {
       } else {
         contents = strings.TrimSpace(string(fc))
       }
+
       touchResult := "ok"
       if err := touch(argFilename); err != nil {
         slog.Error("touch error", "err", err)
         touchResult = "failed"
       }
+
       if !firstrun && lastcontent != contents {
         slog.Warn("content changed", "last", lastcontent, "now", contents)
       }
+
       slog.Info("fileinfo:", "name", fi.Name(), "size", fi.Size(), "mode", fi.Mode(), "touch", touchResult,  "content", contents )
+      
       lastcontent = contents
+      
+      // TODO: this is a mess and loading the file content into memory 
+      // will absolutely not work in a general sense. This was ok in the 
+      // domain specific application of a single, 7 byte file, but will 
+      // need to be refactored out for general release 
+      // some options:
+      //   a) use the file system for the reversion content, we could 
+      //      create a "snapshot" of the watched file in the same path 
+      //      with some specific suffix like <file>.fiwatched.snap 
+      //      then fiwatcherd could update it when it detects a change 
+      //      and write from it when it detects truncation 
+      //   b) <file>.fiwatcherd.snap files coud be kept in /tmp/ but 
+      //      but behave the same as point a)
       if contents != "" {
         lastgoodcontent = lastcontent
       } else {
@@ -122,6 +152,9 @@ func main() {
           if flagIncrement {
             writeContent, err = incrementFileContent(lastgoodcontent)
             if err != nil {
+              //TODO: refactor out domain-specific functionality assumptions
+              //      we assume that it's always a valid int here and this 
+              //      err remains unhandled 
               slog.Error("can't increment content", "err", err)
               writeContent = ""
             }
@@ -141,6 +174,10 @@ func main() {
   }
 }
 
+// TODO: refactor this entire function to only act if the 
+// content can actally be parsed into an int.
+// this is very domain specific functionality that should 
+// be generalized in the real release of this tool.
 func incrementFileContent(input string) (string, error) {
   intVal, err := strconv.Atoi(input)
   if err != nil {
@@ -156,6 +193,9 @@ func incrementFileContent(input string) (string, error) {
 
   return strVal, nil 
 }
+
+// TODO: refactor this functionality to truncate the output to 
+// something that is sane to put in a log line
 func putStringToFile(filePath string, contents string) error {
   f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
   defer f.Close()
@@ -170,6 +210,9 @@ func putStringToFile(filePath string, contents string) error {
   return nil 
 }
 
+// TODO: this should have two behaviours: a) a quasi-touch
+// which just tries to open the file; an b) a full-touch 
+// which not onlt opens the file but also updates the modtime
 func touch(filePath string) error {
 	f, err := os.OpenFile(filePath, os.O_CREATE, 0600)
   defer f.Close()
@@ -179,8 +222,3 @@ func touch(filePath string) error {
 	currentTime := time.Now().Local()
 	return os.Chtimes(filePath, currentTime, currentTime)
 }
-//
-// func main() {
-// 	fileName := "temp.txt"
-// 	fmt.Println(touch(fileName))
-// }
